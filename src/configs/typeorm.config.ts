a48a2d5277182c6c6d5be51bb8bdd30d1f09fb8a
양민준 <97660495+gwangminjun@users.noreply.github.com>
2023-07-27 23:03:59 +0900

    첫 init
    npm 구성 및 db 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,33 +1,23 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 //import 'dotenv/config';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
 
-if(process.env.NODE_ENV === 'prod'){
-    dotenv.config({ path: '.env.prod' });
-}else if(process.env.NODE_ENV === 'dev'){
-    dotenv.config({ path: '.env.dev' });
-}else{
-    dotenv.config({ path: '.env.local' });
-}
+dotenv.config({ path: '.env.dev' });
 
-export const typeormConfig : TypeOrmModuleOptions = {
-    type : 'postgres',
-    host : process.env.DB_HOST,
-    port : parseInt(process.env.DB_PORT),
-    username : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
-    entities : [join(__dirname,process.env.ENTITY_PATH)],
-    synchronize : true,
-    logging : true
-}
+export const typeormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [join(__dirname, process.env.ENTITY_PATH)],
+  synchronize: true,
+  logging: true,
+};
 
 // 엔티티를 이용해서 데이터베이스 테이블을 생성함.
 // 엔티티 파일이 어디에 있는지 설정을 해준다.
 // typeorm-model-generator
-// 
-
-
-
-
+//
